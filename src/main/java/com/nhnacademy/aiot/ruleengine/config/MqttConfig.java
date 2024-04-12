251eefb49392e38ab25d7d95e62fa153a0cab493
woooyas <njmin8@naver.com>
2024-04-12 10:29:26 +0900

    Refactor: 패키지 위치 변경

--- src/main/java/com/nhnacademy/aiot/ruleengine/config/MqttConfig.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/config/MqttConfig.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.aiot.ruleengine.occupancy.config;
+package com.nhnacademy.aiot.ruleengine.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
