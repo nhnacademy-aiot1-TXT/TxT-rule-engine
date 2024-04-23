cdfc52f94b99e94314265bc0c1a6df1d0bda4511
woooyas <njmin8@naver.com>
2024-04-23 10:22:09 +0900

    Feat: 재실 관련 rule 일부 구현

--- src/main/java/com/nhnacademy/aiot/ruleengine/config/MqttConfig.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/config/MqttConfig.java
@@ -14,8 +14,6 @@ import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
-import java.util.Objects;
-
 
 /**
  * MQTT와 관련된 설정을 정의하는 클래스
@@ -25,58 +23,52 @@ import java.util.Objects;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
+@SuppressWarnings("ConstantConditions")
 public class MqttConfig {
 
     private final InfluxService influxService;
     private final MessageService messageService;
 
     @Bean
-    public MessageChannel txtSensorInputChannel() {
+    public MessageChannel influxInputChannel() {
         return new DirectChannel();
     }
 
     @Bean
-    public MessageChannel academySensorInputChannel() {
+    public MessageChannel vocChannel() {
         return new DirectChannel();
     }
 
-    /**
-     * 이 메소드는 TxT 팀이 별도로 설치한 센서 메시지를 수신하는 데 필요한 설정을 정의하며,
-     * 수신된 메시지는 txtSensorInputChannel을 통해 전달됩니다.
-     *
-     * @return MessageProducer 객체
-     */
     @Bean
-    public MessageProducer txtSensorInbound() {
+    public MessageChannel occupancyChannel() {
+        return new DirectChannel();
+    }
+
+
+    private MqttPahoMessageDrivenChannelAdapter createMqttAdapter(String url, String clientId, String... topic) {
         MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter("tcp://133.186.229.200:1883", "rule-engine-txt",
-                        "milesight/s/nhnacademy/b/gyeongnam/p/+/d/+/e/+");
+                new MqttPahoMessageDrivenChannelAdapter(url, clientId, topic);
         adapter.setCompletionTimeout(5000);
         adapter.setConverter(new DefaultPahoMessageConverter());
         adapter.setQos(2);
-        adapter.setOutputChannel(txtSensorInputChannel());
         return adapter;
     }
 
-    /**
-     * 이 메소드는 학원의 기존 센서들의 메시지를 수신하는 데 필요한 설정을 정의
-     *
-     * @return MessageProducer 객체
-     */
     @Bean
-    public MessageProducer academySensorInbound() {
-        MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter("tcp://133.186.153.19:1883", "rule-engine-academy",
-                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/co2",
-                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/tvoc",
-                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/humidity",
-                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/temperature",
-                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/illumination",
-                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/battery_level");
-        adapter.setCompletionTimeout(5000);
-        adapter.setConverter(new DefaultPahoMessageConverter());
-        adapter.setQos(2);
-        adapter.setOutputChannel(academySensorInputChannel());
+    public MessageProducer occupancySensorInbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter = createMqttAdapter("tcp://133.186.229.200:1883",
+                "rule-engine-occupancy",
+                "milesight/s/nhnacademy/b/gyeongnam/p/+/d/+/e/occupancy");
+        adapter.setOutputChannel(occupancyChannel());
+        return adapter;
+    }
+
+    @Bean
+    public MessageProducer vocSensorInbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter = createMqttAdapter("tcp://133.186.229.200:1883",
+                "rule-engine-voc",
+                "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/tvoc");
+        adapter.setOutputChannel(vocChannel());
         return adapter;
     }
 
@@ -87,33 +79,55 @@ public class MqttConfig {
      * @return MessageHandler 객체
      */
     @Bean
-    @ServiceActivator(inputChannel = "txtSensorInputChannel")
+    @ServiceActivator(inputChannel = "influxInputChannel")
     public MessageHandler handler1() {
         return message -> {
             String topic = message.getHeaders().get("mqtt_receivedTopic", String.class);
             String payload = message.getPayload().toString();
 
             influxService.save(topic, payload);
-            messageService.sendValidateMessage(Objects.requireNonNull(topic), payload);
+            messageService.sendValidateMessage(topic, payload);
         };
     }
 
-    /**
-     * 이 메소드는 학원의 기존 센서들의 MQTT 메시지를 InfluxDB에 저장합니다.
-     * 하지만 각 센서의 배터리 레벨은 RabbitMQ에도 저장 됩니다. (낮은 배터리 레벨을 탐지하기 위한 데이터 저장)
-     *
-     * @return MessageHandler 객체
-     */
-    @Bean
-    @ServiceActivator(inputChannel = "academySensorInputChannel")
-    public MessageHandler handler2() {
-        return message -> {
-            String topic = message.getHeaders().get("mqtt_receivedTopic", String.class);
-            String payload = message.getPayload().toString();
-
-            if(Objects.requireNonNull(topic).contains("battery_level") || topic.contains("temperature") || topic.contains("humidity") || topic.contains("total_people_count"))
-                messageService.sendValidateMessage(topic, payload);
-            influxService.save(topic, payload);
-        };
-    }
+    //
+//    /**
+//     * 이 메소드는 TxT 팀이 별도로 설치한 센서 메시지를 수신하는 데 필요한 설정을 정의하며,
+//     * 수신된 메시지는 txtSensorInputChannel을 통해 전달됩니다.
+//     *
+//     * @return MessageProducer 객체
+//     */
+//    @Bean
+//    public MessageProducer txtSensorInbound() {
+//        MqttPahoMessageDrivenChannelAdapter adapter =
+//                new MqttPahoMessageDrivenChannelAdapter("tcp://133.186.229.200:1883", "rule-engine-txt",
+//                        "milesight/s/nhnacademy/b/gyeongnam/p/+/d/+/e/total_people_count",
+//                        "milesight/s/nhnacademy/b/gyeongnam/p/+/d/+/e/magnet_status",
+//                        "milesight/s/nhnacademy/b/gyeongnam/p/+/d/+/e/battery_level");
+//        adapter.setCompletionTimeout(5000);
+//        adapter.setConverter(new DefaultPahoMessageConverter());
+//        adapter.setQos(2);
+//        adapter.setOutputChannel(dataInputChannel());
+//        return adapter;
+//    }
+//
+//    /**
+//     * 이 메소드는 학원의 기존 센서들의 메시지를 수신하는 데 필요한 설정을 정의
+//     *
+//     * @return MessageProducer 객체
+//     */
+//    @Bean
+//    public MessageProducer academySensorInbound() {
+//        MqttPahoMessageDrivenChannelAdapter adapter =
+//                new MqttPahoMessageDrivenChannelAdapter("tcp://133.186.153.19:1883", "rule-engine-academy",
+//                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/co2",
+//                     ,
+//                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/humidity",
+//                        "data/s/nhnacademy/b/gyeongnam/p/+/d/+/e/battery_level");
+//        adapter.setCompletionTimeout(5000);
+//        adapter.setConverter(new DefaultPahoMessageConverter());
+//        adapter.setQos(2);
+//        adapter.setOutputChannel(dataInputChannel());
+//        return adapter;
+//    }
 }
---
+++ src/main/java/com/nhnacademy/aiot/ruleengine/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.nhnacademy.aiot.ruleengine.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis 관련 설정
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Long> longRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, Float> floatRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Float> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+}
