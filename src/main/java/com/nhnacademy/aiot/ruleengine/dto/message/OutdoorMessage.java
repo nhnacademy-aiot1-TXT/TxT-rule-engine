c12587ae911a1f140b4ce64f3e3b89dd2193f027
jjunho50 <jjunho50@gmail.com>
2024-04-25 11:02:30 +0900

    Feat : OutdoorMessage 추가, TTL 0초로 변경

--- src/main/java/com/nhnacademy/aiot/ruleengine/dto/message/OutdoorMessage.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/dto/message/OutdoorMessage.java
@@ -2,13 +2,13 @@ package com.nhnacademy.aiot.ruleengine.dto.message;
 
 import lombok.*;
 
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class PredictMessage {
+public class OutdoorMessage {
     private Long time;
     private FloatMessage temperatureMessage;
-    private FloatMessage humidityMessage;
-    private IntegerMessage totalPeopleCountMessage;
+    private FloatMessage HumidityMessage;
 }
