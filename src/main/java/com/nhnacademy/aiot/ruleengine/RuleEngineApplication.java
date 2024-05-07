1c3d18da9e267ff1cc21163e473437208eb56dfe
지민 <njmin8@naver.com>
2024-05-07 15:17:59 +0900

    Update RuleEngineApplication.java

--- src/main/java/com/nhnacademy/aiot/ruleengine/RuleEngineApplication.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/RuleEngineApplication.java
@@ -3,7 +3,7 @@ package com.nhnacademy.aiot.ruleengine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.integration.config.EnableIntegration;
 
