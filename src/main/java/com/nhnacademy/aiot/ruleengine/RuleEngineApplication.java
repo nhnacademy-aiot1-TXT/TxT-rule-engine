b43c3576f3aa809103872f4bb1749a278043db5c
지민 <njmin8@naver.com>
2024-05-07 14:52:45 +0900

    Fix: 어노테이션 변경

--- src/main/java/com/nhnacademy/aiot/ruleengine/RuleEngineApplication.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/RuleEngineApplication.java
@@ -3,19 +3,19 @@ package com.nhnacademy.aiot.ruleengine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.integration.config.EnableIntegration;
 
 @EnableIntegration
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class RuleEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RuleEngineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RuleEngineApplication.class, args);
+    }
 
 }
