1665a1d4dc869131d5e692601ab49905f28b2025
지민 <njmin8@naver.com>
2024-05-10 10:38:33 +0900

    Feat: TimeIntervalResponse dto 구현

---
+++ src/main/java/com/nhnacademy/aiot/ruleengine/dto/TimeIntervalResponse.java
@@ -0,0 +1,18 @@
+package com.nhnacademy.aiot.ruleengine.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TimeIntervalResponse {
+    private Long sensorId;
+    private String sensorName;
+    private LocalTime begin;
+    private LocalTime end;
+}
