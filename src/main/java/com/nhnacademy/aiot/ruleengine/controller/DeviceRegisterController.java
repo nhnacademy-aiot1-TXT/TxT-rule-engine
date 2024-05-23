5aa44cbfeec62801ef4768361af805d258f9c17d
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-05-23 10:33:25 +0900

    Chore: 디바이스 추가 반환타입 변경

--- src/main/java/com/nhnacademy/aiot/ruleengine/controller/DeviceRegisterController.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/controller/DeviceRegisterController.java
@@ -1,10 +1,8 @@
 package com.nhnacademy.aiot.ruleengine.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.aiot.ruleengine.dto.rule.RuleDto;
 import com.nhnacademy.aiot.ruleengine.service.DeviceRegisterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class DeviceRegisterController {
     private final DeviceRegisterService deviceRegisterService;
 
-    private final ObjectMapper objectMapper;
-    private final MessageConverter messageConverter;
-
     @PostMapping(value = "/device/register")
-    public ResponseEntity<Void> registerRuleInfoFlow(@RequestBody String deviceRegisterInfo) {
-        RuleDto ruleDto = deviceRegisterService.parseDeviceRegisterInfo(deviceRegisterInfo);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> registerRuleInfoFlow(@RequestBody String deviceRegisterInfo) {
+        try {
+            RuleDto ruleDto = deviceRegisterService.parseDeviceRegisterInfo(deviceRegisterInfo);
+
+            //Flow 등록 구현
+
+            return ResponseEntity.ok("Flow registered successfully!");
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Flow registration failed: " + e.getMessage());
+        }
     }
 
 }
