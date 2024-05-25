bba61d34d9cd458864c6884dd2f11b0b208a5835
지민 <njmin8@naver.com>
2024-05-25 22:13:01 +0900

    Feat: flow 등록 메소드 추가

--- src/main/java/com/nhnacademy/aiot/ruleengine/controller/DeviceRegisterController.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/controller/DeviceRegisterController.java
@@ -2,6 +2,7 @@ package com.nhnacademy.aiot.ruleengine.controller;
 
 import com.nhnacademy.aiot.ruleengine.dto.rule.RuleInfo;
 import com.nhnacademy.aiot.ruleengine.service.RuleRegisterService;
+import com.nhnacademy.aiot.ruleengine.service.RuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/rule")
 public class DeviceRegisterController {
     private final RuleRegisterService deviceRegisterService;
+    private final RuleService ruleService;
 
     @PostMapping(value = "/device/register")
     public ResponseEntity<String> registerRuleInfoFlow(@RequestBody String deviceRegisterInfo) {
@@ -21,6 +23,7 @@ public class DeviceRegisterController {
             RuleInfo ruleInfo = deviceRegisterService.parseDeviceRegisterInfo(deviceRegisterInfo);
 
             //Flow 등록 구현
+            ruleService.updateRule(ruleInfo);
 
             return ResponseEntity.ok("Flow registered successfully!");
         } catch (Exception e) {
