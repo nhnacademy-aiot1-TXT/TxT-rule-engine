8644ecef67ca9c5dd673e99d81ffa184e5c2a4e3
지민 <njmin8@naver.com>
2024-05-25 22:10:09 +0900

    Feat: awaitility dependency 추가

--- src/main/java/com/nhnacademy/aiot/ruleengine/adapter/CommonAdapter.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/adapter/CommonAdapter.java
@@ -12,18 +12,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "common-api", url = "http://133.186.208.129:8400")
 public interface CommonAdapter {
-    //    @GetMapping("/api/common/device")
-    //    DeviceResponse getDeviceByName(@RequestParam("name") String name);
-    //
-    //    @GetMapping("/api/common/device/devices")
-    //    List<DeviceResponse> getDeviceList();
-
-    @PutMapping("/api/common/device-sensor")
-    ResponseEntity<DeviceSensorResponse> updateSensorByDeviceAndSensor(@RequestBody DeviceSensorRequest deviceSensorRequest);
-
     @GetMapping("/api/common/time-interval")
     TimeIntervalResponse getTimeIntervalBySensorName(@RequestParam("sensorName") String sensorName);
 
-    //    @GetMapping("/api/common/device-sensor/{deviceId}/{sensorId}")
-    //    DeviceSensorResponse getSensorByDeviceAndSensor(@PathVariable("deviceId") Long deviceId, @PathVariable("sensorId") Long sensorId);
+    @PutMapping("/api/common/device-sensor")
+    ResponseEntity<DeviceSensorResponse> updateSensorByDeviceAndSensor(@RequestBody DeviceSensorRequest deviceSensorRequest);
 }
