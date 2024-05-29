45cf8713df708c75dd62448c0e2aa166621e5305
지민 <njmin8@naver.com>
2024-05-29 15:42:04 +0900

    Feat: MqttInInfo serializer, deserializer 추가

--- src/main/java/com/nhnacademy/aiot/ruleengine/dto/rule/MqttInInfo.java
+++ src/main/java/com/nhnacademy/aiot/ruleengine/dto/rule/MqttInInfo.java
@@ -1,12 +1,23 @@
 package com.nhnacademy.aiot.ruleengine.dto.rule;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.KeyDeserializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.nhnacademy.aiot.ruleengine.constants.Constants;
 import lombok.*;
+
+import java.io.IOException;
 
 @Getter
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@JsonSerialize(keyUsing = MqttInInfoKeySerializer.class)
+@JsonDeserialize(keyUsing = MqttInInfoKeyDeserializer.class)
 public class MqttInInfo {
     private String mqttUrl;
     private String topic;
@@ -19,3 +30,19 @@ public class MqttInInfo {
         return getTopic().split("/")[Constants.MEASUREMENT_INDEX];
     }
 }
+
+class MqttInInfoKeySerializer extends JsonSerializer<MqttInInfo> {
+    @Override
+    public void serialize(MqttInInfo value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeFieldName(value.getMqttUrl() + "|" + value.getTopic());
+    }
+}
+
+class MqttInInfoKeyDeserializer extends KeyDeserializer {
+    @Override
+    public Object deserializeKey(String key, DeserializationContext ctxt){
+        String[] parts = key.split("\\|");
+        return new MqttInInfo(parts[0], parts[1]);
+    }
+}
+
